722f92fb1f59adcec1360217cfa5be148fb2d11c
zhufengnodejs <zhufengnodejs@126.com>
2017-05-19 11:35:47 +0800

    3.增加todos状态并进行过滤显示

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,11 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            todos: [{
+                id: Date.now(),
+                title: '今天学习React',
+                completed: false
+            }],
             filter: filters.ALL, //显示全部的待办事项
             editTodo: null,      //正在编辑中的todo
             newTodo: ''          //新的待办事项
@@ -15,13 +20,33 @@ export default class App extends React.Component {
     }
 
     render() {
+        let showTodos = this.state.todos.filter(todo => {
+            switch (this.state.filter) {
+                case filters.COMPLETED:
+                    return todo.completed;
+                case filters.COMPLETED:
+                    return !todo.completed;
+                default:
+                    return true;
+            }
+        },this);
+        let todoItems = showTodos.map((todo,index)=>(
+            <TodoItem key={index} todo = {todo}/>
+        ))
+        let main = (
+            <div className="panel-body">
+                <ul className="list-group">
+                    {todoItems}
+                </ul>
+            </div>
+        )
         return (
             <div className="container" style={{marginTop: 10}}>
                 <div className="row">
                     <div className="col-md-6 col-md-offset-3">
                         <div className="panel panel-default">
                             <TodoHeader/>
-                            <TodoItem/>
+                            {main}
                             <TodoFooter/>
                         </div>
                     </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,19 +2,15 @@ import React from 'react';
 export default class TodoItem extends React.Component {
     render(){
         return (
-            <div className="panel-body">
-                <ul className="list-group">
-                    <li className="list-group-item">
-                        <div className="row">
-                            <div className="col-xs-1"><input type="checkbox"/></div>
-                            <div className="col-xs-10"><span>在珠峰学习React</span></div>
-                            <div className="col-xs-1">
-                                <button className="btn btn-danger btn-xs">X</button>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+            <li className="list-group-item">
+                <div className="row">
+                    <div className="col-xs-1"><input type="checkbox" checked={this.props.todo.completed}/></div>
+                    <div className="col-xs-10"><span>{this.props.todo.title}</span></div>
+                    <div className="col-xs-1">
+                        <button className="btn btn-danger btn-xs">X</button>
+                    </div>
+                </div>
+            </li>
         )
     }
 }
